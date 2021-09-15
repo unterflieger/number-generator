ed2e0560ec4dd83345626dfa5b2de0b932843add
unterflieger <unterflieger010@gmail.com>
2021-09-15 22:24:48 +0200

    catch KeyboardInterrupt

--- Random Number.py
+++ Random Number.py
@@ -5,52 +5,57 @@ import random
 def cls():
     _ = system('cls')
 
-while True:
+try:
     while True:
-        cls()
-        print('Random Number Generator\n')
-        try:
-            min = int(input('Minimum: '))
-            if(min >= 0):
-                try:
-                    max = int(input('Maximum: '))
-                    if(max >= 0):
-                        if(min <= max):
-                            break
+        while True:
+            cls()
+            print('Random Number Generator\n')
+            try:
+                min = int(input('Minimum: '))
+                if(min >= 0):
+                    try:
+                        max = int(input('Maximum: '))
+                        if(max >= 0):
+                            if(min <= max):
+                                break
+                            else:
+                                print("min is langer than max")
+                                sleep(1)
                         else:
-                            print("min is langer than max")
+                            print("Please enter a positiv integer")
                             sleep(1)
-                    else:
-                        print("Please enter a positiv integer")
+                    except ValueError:
+                        print("That is not an integer.")
                         sleep(1)
-                except ValueError:
-                    print("That is not an integer.")
+                else:
+                    print("Please enter a positiv integer")
                     sleep(1)
-            else:
-                print("Please enter a positiv integer")
+            except ValueError:
+                print("That is not an integer.")
                 sleep(1)
-        except ValueError:
-            print("That is not an integer.")
-            sleep(1)
 
 
-    rand_int = (random.randint(int(min),int(max)))
-    cut = 0
-    x = ""
-    cls()
-    
-    if(len(str(rand_int)) > 4):
-        cut = len(str(rand_int)) - 4
-        rand_int_cut = int(str(rand_int)[:-cut])
-    else:
-        rand_int_cut = rand_int
-
-    for i in range(rand_int_cut):
+        rand_int = (random.randint(int(min),int(max)))
+        cut = 0
         x = ""
-        for not_used in range(cut):
-            x += str(random.randint(1, 9))
-        print(str(i) + str(x), end='\r')
-        sleep(0.01)
+        cls()
+        
+        if(len(str(rand_int)) > 4):
+            cut = len(str(rand_int)) - 4
+            rand_int_cut = int(str(rand_int)[:-cut])
+        else:
+            rand_int_cut = rand_int
 
-    print(rand_int)
-    input("Press Enter to continue...")
+        for i in range(rand_int_cut):
+            x = ""
+            for not_used in range(cut):
+                x += str(random.randint(1, 9))
+            print(str(i) + str(x), end='\r')
+            sleep(0.01)
+
+        print(rand_int)
+        input("Press Enter to continue...")
+
+except KeyboardInterrupt:
+    cls()
+    print("\33[34mThsnks for using RndNmbrGen\33[0m")
