0018c5a3f1ec49eb203d252e7bf736ac8c1c5015
unterflieger <unterflieger010@gmail.com>
2021-09-15 22:01:29 +0200

    Code overhaul

--- Random Number.py
+++ Random Number.py
@@ -6,26 +6,57 @@ def cls():
     _ = system('cls')
 
 while True:
-    cls()
-    print('Random Number Generator')
-    print('')
-    min = input('Minimum: ')
-    max = input('Maximum: ')
-    a = (random.randint(int(min),int(max)))
-    b = 0
+    while True:
+        cls()
+        print('Random Number Generator\n')
+        try:
+            min = int(input('Minimum: '))
+            try:
+                max = int(input('Maximum: '))
+                if(min <= max):
+                    break
+                else:
+                    print("min is larger than max")
+                    sleep(1)
+            except ValueError:
+                print("That is not an integer.")
+                sleep(1)
+        except ValueError:
+            print("That is not an integer.")
+            sleep(1)
+
+
+    rand_int = (random.randint(int(min),int(max)))
+    cut = 0
+    x = ""
     cls()
     
-    while a > b:
-        print(b)
-        if a - 10 > b:
-            b = (b)+10
-            if a is b:
-                sleep(0.1)
-        else:
-            b = (b)+1
-            sleep(0.04)
-        cls()
+    if(len(str(rand_int)) > 4):
+        cut = len(str(rand_int)) - 4
+        rand_int_cut = int(str(rand_int)[:-cut])
+    else:
+        rand_int_cut = rand_int
 
-    cls()
-    print(a)
+    for i in range(rand_int_cut):
+        x = ""
+        for not_used in range(cut):
+            x += str(random.randint(1, 9))
+        print(str(i) + str(x), end='\r')
+        sleep(0.01)
+
+    print(rand_int)
     input("Press Enter to continue...")
+
+
+    
+#    while a > b:
+#        print(b, end='\r')
+#        if a - 10 > b:
+#            b = (b)+10
+#            if a is b:
+#                sleep(0.1)
+#        else:
+#            b = (b)+1
+#            sleep(0.04)
+
+#   cls()
